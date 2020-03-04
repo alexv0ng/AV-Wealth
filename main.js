5b2a55bc1a2c61f92ba25b0ad58278594348f191
Alex Vong <avong003@gmail.com>
2020-03-04 09:03:02 -0800

    deleted commented code

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
-// Initialize and add the map
+//Google Map
 function initMap() {
-	// Your location
 	const loc = { lat: 34.149551, lng: -118.141449 };
-	// Centered map on location
+
 	const map = new google.maps.Map(document.querySelector('.google-map'), {
 		zoom   : 14,
 		center : loc
 	});
-	// The marker, positioned at location
+
 	const marker = new google.maps.Marker({ position: loc, map: map });
 }
 
-// Sticky menu background
 window.addEventListener('scroll', function() {
 	if (window.scrollY > 150) {
 		document.querySelector('#navbar').style.opacity = 0.9;
